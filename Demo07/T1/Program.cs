98f5dc4aed1c4cd7666ce7796a7826b79b3acdad
AlbKuu <albert.kuusjarvi@gmail.com>
2016-02-02 11:08:48 +0200

    T2 rikki

---
+++ Demo07/T1/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+* Created: 2.2.2016 
+* Authors: Albert Kuusjärvi
+*
+*Tehtävä 1 - Tiedostoon kirjoittaminen ja lukeminen
+*Tee ohjelma, joka kirjoittaa käyttäjän antamat merkkijonot tiedostoon (lopetusehdon voit päättää itse) ja sulje tiedosto. 
+*Avaa tämän jälkeen tiedosto lukemista varten ja tulosta näyttölaitteelle tiedoston sisältö riveittäin.
+*/
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = null;
+            StreamWriter outputFile = new StreamWriter(@"d:\K2021\Olio\Demo07\text.txt");
+            //käyttäjältä kysytään rivejä
+            do
+            {
+                Console.Write("Give a text line (enter stops) : ");
+                line = Console.ReadLine();
+                outputFile.WriteLine(line); //kirjoitetaan levylle!
+            } while (line.Length!=0);
+            //suljetaan tiedosto
+            outputFile.Close();
+
+            try{
+                //avaa tiedosto
+                string[] lines = File.ReadAllLines(@"d:\K2021\Olio\Demo07\text.txt");
+                //näytä sisältö
+                foreach (string line2 in lines)
+                {
+                    Console.WriteLine(line2);
+                }
+            }catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found!");
+            }
+        }
+    }
+}
---
+++ Demo07/T2/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+* Created: 2.2.2016 
+* Authors: Albert Kuusjärvi
+*
+Tehtävä 2 - luvut tiedostoihinhome Kotitehtävä
+
+Tee ohjelma, joka kysyy käyttäjältä lukuja (joko kokonaisluku tai reaaliluku) ja tallenna kokonaisluvut eri tiedostoon kuin reaaliluvut. 
+Sovellus tulee lopettaa, jos käyttäjä ei syötä kokonais- tai reaalilukua. Tarkista tiedostojen sisältö jollain tekstieditorilla. 
+*/
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            int number;
+            double real;
+           // StreamWriter KokoFile = new StreamWriter(@"d:\K2021\Olio\Demo07\kokonais.txt");
+            //StreamWriter RealiFile = new StreamWriter(@"d:\K2021\Olio\Demo07\reali.txt");
+            do
+            {
+                Console.WriteLine("Give a number(enter ends)");
+                line = Console.ReadLine();
+
+                bool result = int.TryParse(line, out number);
+                if (result)
+                {
+                    StreamWriter KokoFile = new StreamWriter(@"d:\K2021\Olio\Demo07\kokonais.txt");
+                    KokoFile.WriteLine(number); //kirjoitetaan levylle!
+                    KokoFile.Close(); //suljetaan tiedosto
+                }
+
+                bool result2 = double.TryParse(line, out real);
+                if (result2)
+                {
+                    StreamWriter RealiFile = new StreamWriter(@"d:\K2021\Olio\Demo07\reali.txt");
+                    RealiFile.WriteLine(number); //kirjoitetaan levylle!
+                    RealiFile.Close(); //suljetaan tiedosto
+                }
+            } while (line.Length != 0);
+            //KokoFile.Close(); //suljetaan tiedosto
+            //RealiFile.Close;
+
+            
+        }
+    }
+}
